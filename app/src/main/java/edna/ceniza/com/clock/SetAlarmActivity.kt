f196f2c471a6fc570befafddff4e617b78be5156
Edna Ceniza <edsceniza@gmail.com>
2018-02-11 22:20:57 +0800

    Clock Midterm

--- app/src/main/java/edna/ceniza/com/clock/SetAlarmActivity.kt
+++ app/src/main/java/edna/ceniza/com/clock/SetAlarmActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import android.widget.Button
 import android.widget.TimePicker
